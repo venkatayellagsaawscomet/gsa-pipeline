1a87d3e21483d62c7f90beb821793876596df867
venkatayellagsaawscomet <venkata.yella@gsa.gov>
2020-09-10 21:20:16 +0000

    updated handler 1. Does the pipeline correctly fire off when we check in code change (base test of the pipeline)? !!!

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,8 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from a Dummy Lambda Function',
-    statusCode: 200,
+    body: 'Hello from a Dummy Lambda Function \n 1. Does the pipeline correctly fire off when we check in code change (base test of the pipeline)? !!!',
+    statusCode: 400,
   };
 }
+
