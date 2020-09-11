6cc1db5a45031623d5a3ebab232b74f662a235e4
venkatayellagsaawscomet <venkata.yella@gsa.gov>
2020-09-11 17:48:31 +0000

    Updating 202009110145PM

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: 'Hello from a Dummy Lambda Function \n 1. Does the pipeline correctly fire off when we check in code change (base test of the pipeline)? !!!',
+    body: 'Hello from a Dummy Lambda Function \n COMPLETED.... 1. Does the pipeline correctly fire off when we check in code change (base test of the pipeline)? !!!',
     statusCode: 400,
   };
 }
